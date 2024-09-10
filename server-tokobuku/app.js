db4d1d6395fa10453f38831e9b8ea659f183b381
Agus27111 <agussetiawanbelajar@gmail.com>
2024-09-11 00:31:51 +0700

    setting up

---
+++ server-tokobuku/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+//route
+const categoriesRouter = require('./app/api/v1/categories/router');
+
+const app = express();
+const URL = '/api/v1'
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to Tokobuku'})
+});
+
+app.use(`${URL}/auth`, categoriesRouter);
+
+module.exports = app;
